e7023f5fb458c44d794d2ac74b913dcbc50ff913
João Macedo <joaoprbmacedo@gmail.com>
2026-05-29 22:46:58 -0300

    feat: tela de pratica e tutorial

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { 
+  IonHeader, IonToolbar, IonTitle, IonContent, 
+  IonCard, IonCardContent, IonItem, IonLabel, 
+  IonBadge, IonIcon 
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { laptopOutline, rocketOutline, folderOpenOutline, globeOutline, checkmarkCircleOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  standalone: true,
+  imports: [
+    IonHeader, IonToolbar, IonTitle, IonContent, 
+    IonCard, IonCardContent, IonItem, IonLabel, 
+    IonBadge, IonIcon
+  ]
 })
 export class Tab3Page {
-  constructor() {}
+  constructor() {
+    // Trocamos o primeiro rocketOutline por laptopOutline
+    addIcons({ laptopOutline, rocketOutline, folderOpenOutline, globeOutline, checkmarkCircleOutline });
+  }
 }
