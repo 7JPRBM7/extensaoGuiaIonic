21197f3a186b3bdd9e78357a0dbc39d44a18683c
João Macedo <joaoprbmacedo@gmail.com>
2026-05-29 22:02:47 -0300

    feat: tela de passo a passo de como baixar o material

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { 
+  IonHeader, IonToolbar, IonTitle, IonContent, 
+  IonCard, IonCardContent, IonItem, IonLabel, 
+  IonIcon, IonBadge 
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { logoNodejs, codeWorkingOutline, terminalOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  standalone: true,
+  imports: [
+    IonHeader, IonToolbar, IonTitle, IonContent, 
+    IonCard, IonCardContent, IonItem, IonLabel, 
+    IonIcon, IonBadge
+  ]
 })
 export class Tab2Page {
-
-  constructor() {}
-
+  constructor() {
+    addIcons({ logoNodejs, codeWorkingOutline, terminalOutline });
+  }
 }
